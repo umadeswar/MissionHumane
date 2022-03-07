30fa5d72f8389308908c61f934a25a8e1cd3cfea
uma <uma@localhost>
2022-03-07 09:30:38 -0500

    Final Commit

---
+++ src/test/java/test/MissionHumane.java
@@ -0,0 +1,112 @@
+package test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.annotations.Test;
+
+
+
+import utils.ExcelUtils;
+
+public class MissionHumane {
+	static WebDriver driver;
+	static int j;
+	static ExcelUtils xlUtils= new ExcelUtils("files/MissionHumane.xlsx");
+	
+	@Test
+	public static void ScrapeWestBengal() throws InterruptedException, IOException {
+		System.setProperty("webdriver.chrome.driver", "/Users/eswar/Documents/chromedriver");
+
+		driver = new ChromeDriver();
+		driver.get("https://westbengal.covidsafe.in/");
+		driver.manage().timeouts().implicitlyWait(10000,TimeUnit.MILLISECONDS);
+			
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+	
+
+		for(int j=1;j<=7;j++) {
+			WebElement loadmore= driver.findElement(By.xpath("//button[@class='btn btn-primary']"));
+			js.executeScript("arguments[0].click();", loadmore);
+		}
+		
+		clickAllRows(js);
+		List<WebElement> row1 =driver.findElements(By.xpath("//table[@class='result-table table table-borderless table-hover']/tbody/tr"));
+		writeData(1, row1.size());
+		driver.close();
+
+	}
+	
+	public static void clickAllRows(JavascriptExecutor js) {
+		List<WebElement> allRows= driver.findElements(By.tagName("strong")); //.xpath("//div[@class='p-1']"));
+		for(WebElement ele : allRows) {
+			js.executeScript("arguments[0].click();", ele);
+		}
+	}
+	
+	public static void writeData(int start, int end) throws IOException {
+	
+
+		int currentRow = start;
+		for (int i = start; i <= end; i++) {
+
+			ArrayList<String> data = new ArrayList<String>();
+			data.add("Hospital Name");
+			data.add("Without Oxygen");
+			data.add("With Oxygen");
+			data.add("ICU Without Ventilator");
+			data.add("ICU With Ventilator");
+			data.add("Last Updated Time");
+			data.add("Phone Number");
+			data.add("Address");
+			xlUtils.writeToExcel(data,"WestBengalSheet",0);
+			data.clear();
+			
+			System.out.println(end);
+			currentRow++;
+			List<WebElement> cols = driver.findElements(By.xpath("//tr[1]/td[@class='text-center']"));
+			System.out.println("Row:" + i);
+
+			if ((i) % 2 != 0) {
+				String row1Content = driver.findElement(By.xpath("//tr[" + i + "]/td/div[@class='p-1']")).getText();// row1.get(i).getText()
+				data.add(row1Content);
+				for (j = 1; j <= cols.size(); j++) {
+					System.out.println("Size:" + cols.size());
+					String a = driver.findElement(By.xpath("//tr[" + i + "]/td[@class='text-center'][" + j + "]"))
+							.getText();
+					System.out.println("Col Value:" + a);
+					data.add(a);
+
+				}
+			}
+		
+
+			//String b = driver.findElement(By.xpath("//table[@class='result-table table table-borderless table-hover']/tbody/tr[" + (i++) + "]/td/div")).getText(); 
+			//System.out.println("Col Value 2:" + b);
+			String updatedTime = driver.findElement(By.xpath("//table[@class='result-table table table-borderless table-hover']/tbody/tr["+(i++)+"]/td/p[1]"))
+					.getText(); 
+			System.out.println("Time"+updatedTime);
+			data.add(updatedTime);
+			String phoneNo = driver.findElement(By.xpath("//table[@class='result-table table table-borderless table-hover']/tbody/tr["+(i) +"]/td/p[2]"))
+					.getText(); 
+			System.out.println("phone"+phoneNo);
+			data.add(phoneNo);
+			String address = driver.findElement(By.xpath("//table[@class='result-table table table-borderless table-hover']/tbody/tr["+(i)+"]/td/p[4]")).getText();
+			data.add(address);
+			System.out.println("address"+address);
+			System.out.println("row inside col" + i);
+			xlUtils.writeToExcel(data, "WestBengalSheet", currentRow);
+		}
+	}
+
+}
+
+
